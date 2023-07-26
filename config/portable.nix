52fff2ccec0989a5a02bcff7f522bbde26e6d4a8
Pikatsuto <pikatsuto@gmail.com>
2023-07-26 21:00:21 +0200

    Add: Laptop Config
    Config File for my Laptop:
    Enable intel_iommu

---
+++ config/portable.nix
@@ -0,0 +1,27 @@
+
+{ config, pkgs, ... }:
+{
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+        gfxmodeEfi = "1920x1080x32";
+        useOSProber = true;
+      };
+    };
+
+    initrd.kernelModules = [
+      "vfio_pci"
+      "vfio"
+      "vfio_iommu_type1"
+      "vfio_virqfd"
+    ];
+
+    kernelParams = [
+      "intel_iommu=on"
+    ];
+  };
+}
