692be7859aa7742ddcb83a0808f68e6cf0b9f945
Pikatsuto <pikatsuto@gmail.com>
2024-03-30 18:05:29 +0100

    fix: winstart

--- config/default/vfio/src/qemuHook.sh
+++ config/default/vfio/src/qemuHook.sh
@@ -2,44 +2,47 @@
 OBJECT="$1"
 OPERATION="$2"
 
-if [[ $OBJECT == "win11" ]]; then
-  case "$OPERATION"
-    in "prepare")
+if [[ "${OBJECT}x" == "win11x" ]]; then
+  case "${OPERATION}x"
+    in "preparex")
       {{ unbindList }}
 
-      umount /run/media/gabriel/WIN11/ 2> /dev/null
-      qemu-nbd --disconnect /dev/nbd0 2> /dev/null
-      rmmod nbd 2> /dev/null
+      umount /run/media/gabriel/WIN11/ 2> /dev/null || true
+      qemu-nbd --disconnect /dev/nbd0 2> /dev/null || true
+      rmmod nbd 2> /dev/null || true
 
       {{ restartDm }}
-      ;;
+    ;;
 
-    "started")
+    "startedx")
       chown {{ username }}:libvirtd /dev/shm/looking-glass
-      ;;
+    ;;
 
-    "release")
+    "releasex")
       {{ bindList }}
 
-      modprobe nbd max_part=8 2> /dev/null
-      qemu-nbd --connect=/dev/nbd0 /var/lib/libvirt/images/win11.qcow2 2> /dev/null
+      modprobe nbd max_part=8 2> /dev/null || true
+      qemu-nbd \
+        --connect=/dev/nbd0 \
+        /var/lib/libvirt/images/win11.qcow2 2> /dev/null || true
 
       {{ restartDm }}
-      ;;
+    ;;
   esac
 
-elif [[ $OBJECT == "win11-no-gpu" ]]; then
-  case "$OPERATION"
-    in "prepare")
-      umount /run/media/gabriel/WIN11/ 2> /dev/null
-      qemu-nbd --disconnect /dev/nbd0 2> /dev/null
-      rmmod nbd 2> /dev/null
-      ;;
+elif [[ "${OBJECT}x" == "win11-no-gpux" ]]; then
+  case "${OPERATION}x"
+    in "preparex")
+      umount /run/media/gabriel/WIN11/ 2> /dev/null || true
+      qemu-nbd --disconnect /dev/nbd0 2> /dev/null || true
+      rmmod nbd 2> /dev/null || true
+    ;;
 
-    "release")
-      modprobe nbd max_part=8
-      qemu-nbd --connect=/dev/nbd0 /var/lib/libvirt/images/win11.qcow2 2> /dev/null
-      ;;
+    "releasex")
+      modprobe nbd max_part=8 2> /dev/null || true
+      qemu-nbd \
+        --connect=/dev/nbd0 \
+        /var/lib/libvirt/images/win11.qcow2 2> /dev/null || true
+    ;;
   esac
 fi
-
