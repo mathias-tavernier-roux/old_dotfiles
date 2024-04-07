2aec35c21964c7b636f13c284aa2f1cdea9f4972
Pikatsuto <pikatsuto@gmail.com>
2024-04-07 03:40:23 +0200

    feat: refactor flake.nix

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     ## ------------------------------------------------------------- ##
-    hosts.url = github:StevenBlack/hosts;
+    hosts.url = "github:StevenBlack/hosts";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     ## ------------------------------------------------------------- ##
     home-manager = {
@@ -30,54 +30,49 @@
     system = "x86_64-linux";
     hostname = "Mathias";
 
-    computer = {
-      hostname = "${hostname}-Fix";
-      vm = {
-        cores = 4;
-        threads = 2;
-        memory = 12;
-        diskSize = 512;
-        diskPath = "/var/lib/libvirt/images";
-        restartDm = false;
-        videoVirtio = false;
-        pcies = false;
-      };
-    };
-    ## ------------------------------------------------------------- ## 
-    laptop = {
-      hostname = "${hostname}-Lap";
-      vm = {
-        cores = 5;
-        threads = 2;
-        memory = 20;
-        diskSize = 128;
-        diskPath = "/home/${username}/VM/Disk";
-        restartDm = false;
-        videoVirtio = false;
-        blacklistPcie = "1002:7480,1002:ab30";
-        pcies = [
-          {
-            pcie = {
-              vmBus = "09";
-              bus = "03";
-              slot = "00";
-              function = "0";
-            };
-            driver = "amdgpu";
-            blacklistDriver = false;
-            blacklistPcie = true;
-          }
-          {
-            pcie = {
-              vmBus = "09";
-              bus = "03";
-              slot = "00";
-              function = "1";
-            };
-            driver = "amdgpu";
-            blacklistDriver = false;
-            blacklistPcie = true;
-          }
+    applyAttrNames = builtins.mapAttrs (name: f: f name);
+
+    computers = applyAttrNames {
+      "${hostname}-Lap" = self: {
+        hostname = "${hostname}-Lap";
+        vm = {
+          cores = 5;
+          threads = 2;
+          memory = 20;
+          diskSize = 128;
+          diskPath = "/home/${username}/VM/Disk";
+          restartDm = false;
+          videoVirtio = false;
+          blacklistPcie = "1002:7480,1002:ab30";
+          pcies = [
+            {
+              pcie = {
+                vmBus = "09";
+                bus = "03";
+                slot = "00";
+                function = "0";
+              };
+              driver = "amdgpu";
+              blacklistDriver = false;
+              blacklistPcie = true;
+            }
+            {
+              pcie = {
+                vmBus = "09";
+                bus = "03";
+                slot = "00";
+                function = "1";
+              };
+              driver = "amdgpu";
+              blacklistDriver = false;
+              blacklistPcie = true;
+            }
+          ];
+        };
+        modules = [
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc
+          nixos-hardware.nixosModules.common-pc-ssd
         ];
       };
     };
@@ -97,32 +92,17 @@
 #######################################################################
   in
   {
-    nixosConfigurations = {
-      ${computer.hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        #### ----------------------------------------------------- ####
-        modules = default_modules ++ [
-          (import ./computer.nix {
-            inherit computer username home-manager;
-          })
-        ];
-      };
-      ### --------------------------------------------------------- ###
-      ${laptop.hostname} = nixpkgs.lib.nixosSystem {
-        inherit system;
-        #### ----------------------------------------------------- ####
-        modules = default_modules ++ [
-          (import ./computer.nix {
-            computer = laptop;
-            inherit username home-manager;
-          })
-          ##### ------------------------------------------------- #####
-          nixos-hardware.nixosModules.common-cpu-intel
-          nixos-hardware.nixosModules.common-pc
-          nixos-hardware.nixosModules.common-pc-ssd
-        ];
-      };
-    };
+    nixosConfigurations = (nixpkgs.lib.genAttrs (builtins.attrNames computers)
+    (name: nixpkgs.lib.nixosSystem {
+      inherit system;
+      #### ----------------------------------------------------- ####
+      modules = default_modules ++ computers."${name}".modules ++ [
+        (import ./computer.nix {
+          computer = computers."${name}";
+          inherit username home-manager;
+        })
+      ];
+    }));
   };
 #######################################################################
 }
