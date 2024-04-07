1dc6b550bf1d79143a4bc3eb1b37a6d514399d44
Pikatsuto <pikatsuto@gmail.com>
2024-04-07 19:46:48 +0200

    fix: use winutils for (u)mount

--- config/default/vm/src/qemuHook.sh
+++ config/default/vm/src/qemuHook.sh
@@ -6,9 +6,7 @@ if [[ "${OBJECT}x" == "win11x" ]]; then
   case "${OPERATION}x"
     in "preparex")
       {{ unbindList }}
-
       winumount 2> /dev/null || true
-
       {{ restartDm }}
     ;;
 
@@ -18,9 +16,7 @@ if [[ "${OBJECT}x" == "win11x" ]]; then
 
     "releasex")
       {{ bindList }}
-
       winmount 2> /dev/null || true
-
       {{ restartDm }}
     ;;
   esac
@@ -28,16 +24,11 @@ if [[ "${OBJECT}x" == "win11x" ]]; then
 elif [[ "${OBJECT}x" == "win11-no-gpux" ]]; then
   case "${OPERATION}x"
     in "preparex")
-      umount /run/media/{{ username }}/WIN11/ 2> /dev/null || true
-      qemu-nbd --disconnect /dev/nbd0 2> /dev/null || true
-      rmmod nbd 2> /dev/null || true
+      winumount 2> /dev/null || true
     ;;
 
     "releasex")
-      modprobe nbd max_part=8 2> /dev/null || true
-      qemu-nbd \
-        --connect=/dev/nbd0 \
-        /var/lib/libvirt/images/win11.qcow2 2> /dev/null || true
+      winmount 2> /dev/null || true
     ;;
   esac
 fi
